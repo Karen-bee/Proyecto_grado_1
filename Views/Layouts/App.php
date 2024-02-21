e59b535399486344935f77149a128fd7f089efcf
Karen-bee <83388330+Karen-bee@users.noreply.github.com>
2024-02-21 00:14:41 -0500

    Delete Views directory

--- Views/Layouts/App.php
+++
@@ -1,9 +0,0 @@
-<?php include '/header.php';?>
-
-    <title>Literagiando | App</title>
-
-    <section class="home-section">
-      <div class="text">Dashboard</div>
-    </section>
-
-<?php include '/footer.php';?>
